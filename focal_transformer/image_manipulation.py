e19a742314a82d56d9a2ebe28920b6e6859106a2
Tanya <tanyach1997@gmail.com>
2022-04-17 20:39:41 +0530

    Update image_manipulation.py

--- focal_transformer/image_manipulation.py
+++ focal_transformer/image_manipulation.py
@@ -23,19 +23,33 @@ class Image:
 
     def window_partition(self, image):
         batch_size, height, width, channels = image.shape
+        if ((height % self.window_size) ! = 0) or ((width %self.window_size) !=0):
+                pad_left = pad_top = 0
+                pad_right = (self.window_size - width % self.window_size) % self.window_size
+                pad_bottom = (self.window_size - height % self.window_size) % self.window_size
+                x = tf.pad(image, (0, 0, pad_left, pad_right, pad_top, pad_bottom))
+
+        new_batch_size, new_height, new_width, new_channels = image.shape
+
         image = tf.reshape(
             image,
             (
-                batch_size,
-                height // self.window_size,
+                new_batch_size,
+                new_height // self.window_size,
                 self.window_size,
-                width // self.window_size,
+                new_width // self.window_size,
                 self.window_size,
-                channels,
+                new_channels,
             ),
         )
         windows = tf.reshape(
             tf.transpose(image, (0, 1, 3, 2, 4, 5)),
-            shape=(-1, self.window_size, self.window_size, channels),
+            shape=(-1, self.window_size, self.window_size, new_channels),
         )
         return windows
+
+    def window_reverse(self, windows, image_height, image_width):
+            B = int(windows.shape[0] / (image_height * image_width / self.window_size / self.window_size))
+            x = tf.reshape(B, image_height // self.window_size, image_width // self.window_size, self.window_size, self.window_size, -1)
+            x = tf.reshape(x.permute(0, 1, 3, 2, 4, 5), B, image_height, image_width, -1)
+            return x
