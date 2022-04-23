1bef740c6501d6ad3721a61ccba81689a0835089
Tanya <tanyach1997@gmail.com>
2022-04-23 23:39:04 +0530

    Update backbone.py

--- focal_transformer/backbone.py
+++ focal_transformer/backbone.py
@@ -1,3 +1,25 @@
 import tensorflow as tf
 
 class WindowAttention(tf.keras.layers.Layer):
+    def __init__(
+        self, dim, window_size, num_heads, focal_window, focal_level, qkv_bias=True, dropout_rate=0.0, **kwargs
+    ):
+        super(WindowAttention, self).__init__(**kwargs)
+        self.dim = dim
+        self.window_size = window_size
+        self.num_heads = num_heads
+        self.focal_window = focal_window
+        self.focal_level = focal_level
+        self.scale = (dim // num_heads) ** -0.5
+        self.qkv = tf.keras.layers.Dense(dim * 3, use_bias=qkv_bias)
+        self.attn_dropout = tf.keras.layers.Dropout(dropout_rate)
+        self.proj = tf.keras.layers.Dense(dim)
+        self.proj_drop = tf.keras.layers.Dropout()
+        self.softmax_layer = tf.keras.layers.Softmax()
+
+    def build(self, input_shape):
+
+        coords_h = tf.range(self.window_size[0]) - self.window_size[0] // 2
+        coords_w = tf.range(self.window_size[1]) - self.window_size[1] // 2        
+        coords_window = tf.stack(tf.meshgrid([coords_h, coords_w]), dim=-1)
+        
